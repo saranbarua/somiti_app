be4adf71e7480a431b71cde27c51a40dded12486
saranbarua <saranbarua2357@gmail.com>
2024-12-29 17:50:09 +0600

    assets added

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,6 +10,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      <Text className="font-bold text-lg my-10 text-sky-500">
+        Welcome Priyam
+      </Text>
       <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
