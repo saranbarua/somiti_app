ffd98841a58a663b150c853decee980e6493e853
saranbarua <saranbarua2357@gmail.com>
2025-01-10 18:35:54 +0600

    icon some deletad

--- constants/images.ts
+++ constants/images.ts
@@ -2,10 +2,6 @@ import onboarding from "@/assets/images/onbarding.jpg";
 import onboarding2 from "@/assets/images/onboarding2.jpg";
 import onboarding3 from "@/assets/images/onboarding3.jpg";
 import avatar from "@/assets/images/avatar.png";
-import newYork from "@/assets/images/new-york.png";
-import japan from "@/assets/images/japan.png";
-import cardGradient from "@/assets/images/card-gradient.png";
-import barChart from "@/assets/images/bar-chart.png";
 import whiteGradient from "@/assets/images/white-gradient.png";
 import check from "@/assets/images/check.png";
 import SignInBanner from "@/assets/images/no-result.png";
@@ -17,10 +13,6 @@ export default {
   onboarding3,
   avatar,
   check,
-  newYork,
-  japan,
-  cardGradient,
-  barChart,
   whiteGradient,
   SignInBanner,
   SignUpBanner,
